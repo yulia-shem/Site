c0b07218fbb58f23c80f1c69c48bf238ca9a795f
Yulia <yulia.shem.17@gmail.com>
2023-10-28 19:54:15 +0300

    Окончательная версия

--- main.py
+++ main.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def hello_world():
     return render_template("Квартира Куинджи.htm")
 
+@app.route("/masterskaya")
+def f_masterskaya():
+    return render_template("Мастерская.htm")
+
+@app.route("/gostinaya")
+def f_gostinaya():
+    return render_template("Гостиная.htm")
+
+@app.route("/kabinet")
+def f_kabinet():
+    return render_template("Кабинет.htm")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Как создать подстраницу:
+# 1) каждая страница = отдельная функция (С УНИКАЛЬНЫМ НАЗВАНИЕМ)
+# 2) html ТОЛЬКО В TEMPLATES
+# 3) прописать УНИКАЛЬНЫЙ route
